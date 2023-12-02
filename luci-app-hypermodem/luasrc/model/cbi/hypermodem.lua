9e80d5f77dc8d15f67ee332654be99e67010f7b2
ling <1042585959@qq.com>
2023-12-02 23:31:46 +0800

    更新配置

--- luci-app-hypermodem/luasrc/model/cbi/hypermodem.lua
+++ luci-app-hypermodem/luasrc/model/cbi/hypermodem.lua
@@ -5,7 +5,7 @@ mp = Map("hypermodem")
 mp.title = translate("Hyper Modem Server")
 mp.description = translate("Modem Server For OpenWrt")
 
-s = mp:section(TypedSection, "service", "Base Setting")
+s = mp:section(TypedSection, "service", translate("Base Setting"))
 s.anonymous = true
 
 enabled = s:option(Flag, "enabled", translate("Enable"))
