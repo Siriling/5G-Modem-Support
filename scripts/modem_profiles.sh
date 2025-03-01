cc44adcf8e0776783a4b186a33fd6c7d6e184aff
Doan Duy <doanduy09@example.com>
2025-03-01 10:23:35 +0700

    Enhance modem_profiles.sh with detailed modem info

--- scripts/modem_profiles.sh
+++ scripts/modem_profiles.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
-# Modem profile database
+# Advanced modem profile database
 get_profile() {
   VID="$1"
   case $VID in
-    "05c6") echo "Quectel: qmi_wwan,option|AT+CSQ|nr5g_band" ;;
-    "1199" | "0f3d") echo "Sierra Wireless: sierra,option|AT!GSTATUS?|band" ;;
-    "1bc7") echo "Telit: qmi_wwan,option|AT#CSQ|band" ;;
-    "12d1") echo "Huawei: cdc_ether,option|AT+CSQ|band" ;;
-    "1e0e") echo "Fibocom: qmi_wwan,option|AT+CSQ|nr5g_band" ;;
-    *) echo "Unknown: qmi_wwan,option|AT+CSQ|band" ;;
+    "05c6") echo "Quectel|qmi_wwan,option|AT+CGMR,AT+CSQ|nr5g_band|QMI" ;;
+    "1199" | "0f3d") echo "Sierra Wireless|sierra,option|AT+CGMR,AT!GSTATUS?|band|MBIM" ;;
+    "1bc7") echo "Telit|qmi_wwan,option|AT+CGMR,AT#CSQ|band|QMI" ;;
+    "12d1") echo "Huawei|cdc_ether,option|AT+CGMR,AT+CSQ|band|PPP" ;;
+    "1e0e") echo "Fibocom|qmi_wwan,option|AT+CGMR,AT+CSQ|nr5g_band|QMI" ;;
+    *) echo "Unknown|qmi_wwan,option|AT+CGMR,AT+CSQ|band|QMI" ;;
   esac
 }
