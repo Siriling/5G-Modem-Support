b0ee7b308c4328919ae77104c128858e7bd4b049
Doan Duy <doanduy09@example.com>
2025-03-01 10:25:42 +0700

    Update main.sh with all advanced features

--- scripts/main.sh
+++ scripts/main.sh
@@ -3,9 +3,13 @@
 ACTION="$1"
 case $ACTION in
   "detect") sh /usr/bin/detect_modem.sh ;;
+  "install") sh /usr/bin/smart_kmod.sh ;;
   "lock") sh /usr/bin/band_lock.sh "$2" ;;
   "diag") sh /usr/bin/modem_diag.sh ;;
-  "connect") sh /usr/bin/modem_connect.sh "$2" ;;
+  "connect") sh /usr/bin/modem_connect.sh ;;
   "auto") sh /usr/bin/auto_reconnect.sh ;;
-  *) echo "Usage: $0 {detect|lock <bands>|diag|connect <protocol>|auto}" ;;
+  "update") sh /usr/bin/firmware_update.sh "$2" ;;
+  "optimize") sh /usr/bin/optimize_network.sh ;;
+  "luci") sh /usr/bin/luci_wwan.sh ;;
+  *) echo "Usage: $0 {detect|install|lock <bands>|diag|connect|auto|update <firmware>|optimize|luci}" ;;
 esac
