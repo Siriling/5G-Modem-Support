fb00d7f6d209370e153b2e309c5536e30e3ec965
Doan Duy <doanduy09@example.com>
2025-03-01 09:51:48 +0700

    Add modem auto-detection script for WWAN cards

--- scripts/detect_modem.sh
+++ scripts/detect_modem.sh
@@ -1,6 +1,2 @@
 #!/bin/sh
-for dev in $(lsusb | awk "{print \$6}"); do
-  vid=$(echo $dev | cut -d: -f1)
-  pid=$(echo $dev | cut -d: -f2)
-  [ -n "$vid" ] && echo "$vid $pid" > /sys/bus/usb-serial/drivers/option1/new_id
-done
+for dev in $(lsusb | awk "{print \$6}"); do vid=$(echo $dev | cut -d: -f1); pid=$(echo $dev | cut -d: -f2); [ -n "$vid" ] && echo "$vid $pid" > /sys/bus/usb-serial/drivers/option1/new_id; done
